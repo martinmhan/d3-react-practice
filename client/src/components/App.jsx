513466c5e4afd0e28442ad729e9eee420488d386
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-26 01:42:29 +0700

    [prog] add view to App state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import DroppingBubbles from './transitions/DroppingBubbles';
 import BrushAndBroom from './brush-and-broom/BrushAndBroom';
 import Scatterplot from './scatterplot/Scatterplot';
-import Barchart from './barchart/Barchart';
+// import Barchart from './barchart/Barchart';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      view: 'scatterplot',
+    };
   }
 
   render = () => (
     <div id="app">
-      {/* <DroppingBubbles /> */}
-      {/* <BrushAndBroom /> */}
-      <Scatterplot />
+      {
+        this.state.view === 'droppingbubbles' ? <DroppingBubbles /> :
+        this.state.view === 'brushandbroom' ? <BrushAndBroom /> :
+        this.state.view === 'scatterplot' ? <Scatterplot /> :
+        null
+      }
     </div>
   );
 }
